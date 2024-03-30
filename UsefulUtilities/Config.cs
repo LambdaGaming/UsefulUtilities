cfc72ab09651b4be6e0c1883362d6425d29707bc
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2024-03-30 02:32:18 -0400

    Add config to disable extended spawn pool

--- UsefulUtilities/Config.cs
+++ UsefulUtilities/Config.cs
@@ -10,5 +10,8 @@ namespace UsefulUtilities
 
 		[Description( "Whether or not debug messages should be shown in the console." )]
 		public bool Debug { get; set; } = false;
+
+		[Description( "Whether or not 096 and 3114 should be added to the regular spawn pool." )]
+		public bool ExtendedSpawnPool { get; set; } = true;
 	}
 }
--- UsefulUtilities/EventHandlers.cs
+++ UsefulUtilities/EventHandlers.cs
@@ -13,12 +13,12 @@ namespace UsefulUtilities
 
 		public void OnPlayerSpawned( SpawnedEventArgs ev )
 		{
-			if ( ev.Player.IsScp && ev.OldRole.Team != Team.SCPs && ev.Player.Role.Type != RoleTypeId.Scp0492 )
+			if ( plugin.Config.ExtendedSpawnPool && ev.Player.IsScp && ev.OldRole.Team != Team.SCPs && ev.Player.Role.Type != RoleTypeId.Scp0492 )
 			{
 				int r = rand.Next( 100 );
-                if ( r < 5 )
+                if ( r < 10 )
 					ev.Player.Role.Set( RoleTypeId.Scp3114, SpawnReason.ForceClass );
-				else if ( r < 10 && ev.Player.GetScpPreference( RoleTypeId.Scp096 ) >= 0 )
+				else if ( r < 20 && ev.Player.GetScpPreference( RoleTypeId.Scp096 ) >= 0 )
                     ev.Player.Role.Set( RoleTypeId.Scp096, SpawnReason.ForceClass );
             }
 		}
