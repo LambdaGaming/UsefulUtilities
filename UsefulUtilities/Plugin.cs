6394a66cec0ff239fbcd976fcbcdb20df2a54ea1
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-01-28 18:51:51 -0500

    Version bump

--- UsefulUtilities/Plugin.cs
+++ UsefulUtilities/Plugin.cs
@@ -8,8 +8,8 @@ namespace UsefulUtilities
 	public class Plugin : Plugin<Config>
 	{
 		private EventHandlers EventHandlers;
-		public override Version Version { get; } = new Version( 1, 2, 0 );
-		public override Version RequiredExiledVersion { get; } = new Version( 9, 4, 0 );
+		public override Version Version { get; } = new Version( 1, 3, 0 );
+		public override Version RequiredExiledVersion { get; } = new Version( 9, 5, 0 );
 		public override string Author { get; } = "OPGman";
 		public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
